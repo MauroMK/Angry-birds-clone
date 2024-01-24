4c03661fdfe26ed1a7641e57fe896396aa70e072
Mauro Moreno <mmauromoreno1@gmail.com>
2024-01-24 19:41:13 -0300

    Slingshot line done

--- Angry birds clone/Assets/Scripts/SlingshotHandler.cs
+++ Angry birds clone/Assets/Scripts/SlingshotHandler.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SlingshotHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private LineRenderer leftLineRenderer;
+    [SerializeField] private LineRenderer rightLineRenderer;
 
-    // Update is called once per frame
+    [SerializeField] private Transform leftStartPosition;
+    [SerializeField] private Transform rightStartPosition;
+
     void Update()
     {
-        
+        if (Mouse.current.leftButton.isPressed)
+        {
+            DrawLines();
+        }
+    }
+
+    private void DrawLines()
+    {
+        // Gets the screen position of the mouse
+        Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        SetLines(touchPosition);
+    }
+
+    private void SetLines(Vector2 position)
+    {
+        leftLineRenderer.SetPosition(0, position);
+        leftLineRenderer.SetPosition(1, leftStartPosition.position);
+
+        rightLineRenderer.SetPosition(0, position);
+        rightLineRenderer.SetPosition(1, rightStartPosition.position);
     }
 }
