f40f07ce483e27bce00294151d19a7c30bdf470b
Mauro Moreno <mmauromoreno1@gmail.com>
2024-03-05 18:51:34 -0300

    Created the pig script

---
+++ Angry birds clone/Assets/Scripts/Pig.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 3f;
+    [SerializeField] private float damageThreshold = 0.2f;
+
+    private float currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void DamageBaddie(float damageAmouth)
+    {
+        currentHealth -= damageAmouth;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //* Damages the pig with the velocity of the other object colliding with him
+        float impactVelocity = other.relativeVelocity.magnitude;
+
+        if (impactVelocity > damageThreshold)
+        {
+            DamageBaddie(impactVelocity);
+        }
+    }
+}
